236bc06a75d11bd1e6be335cc40a3fc63e579a5f
LeonardoDM <leonardodiasmoraes@hotmail.com>
2020-03-20 18:33:42 -0300

    Updates

---
+++ precache-manifest.1f04da7418f68188b60da860bc520b47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1211ea3812c338737198498a0f375ebe",
+    "url": "/reactjs-book-finder/index.html"
+  },
+  {
+    "revision": "1ac778985d4b735bd592",
+    "url": "/reactjs-book-finder/static/css/main.0ae7ea35.chunk.css"
+  },
+  {
+    "revision": "802bccd6750c03454f8c",
+    "url": "/reactjs-book-finder/static/js/2.242d1ff9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-book-finder/static/js/2.242d1ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ac778985d4b735bd592",
+    "url": "/reactjs-book-finder/static/js/main.9bc9b363.chunk.js"
+  },
+  {
+    "revision": "2cb3fc7bd99edea8c242",
+    "url": "/reactjs-book-finder/static/js/runtime-main.8786df75.js"
+  }
+]);
